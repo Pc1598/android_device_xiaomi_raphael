da679249af4ad14c9c2dd6613a7eb22d12536acc
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2023-05-02 10:43:23 +0000

    raphael: Add partitions and recovery flags * inherit Fstab * Set Crypto Props accordingly from https://github.com/xiaomi-sm8150-devs/android_device_xiaomi_sm8150-common
    Co-Authored-By: Pc1598 <piyushchouhan1598@gmail.com>

--- device.mk
+++ device.mk
@@ -7,6 +7,23 @@
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
+# Crypto
+PRODUCT_VENDOR_PROPERTIES += \
+    ro.crypto.dm_default_key.options_format.version=2 \
+    ro.crypto.volume.filenames_mode=aes-256-cts \
+    ro.crypto.volume.metadata.method=dm-default-key \
+    ro.crypto.volume.options=::v2
+
+# Init
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/init/fstab.qcom:$(TARGET_COPY_OUT_RAMDISK)/fstab.qcom
+
+PRODUCT_PACKAGES += \
+    fstab.qcom
+
+# Partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
 # Platform
 TARGET_BOARD_PLATFORM := msmnile
 
