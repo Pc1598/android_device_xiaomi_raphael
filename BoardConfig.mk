19a277874343fe24fe9b46527f110397b4d5c326
Piyush Raj Chouhan <103498040+Pc1598@users.noreply.github.com>
2023-05-25 10:24:05 +0530

    raphael: Initial device tree makefiles

---
+++ BoardConfig.mk
@@ -0,0 +1,9 @@
+#
+# Copyright (C) 2023 Paranoid Android
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+BOARD_VENDOR := xiaomi
+
+DEVICE_PATH := device/xiaomi/raphael
---
+++ device.mk
@@ -0,0 +1,9 @@
+#
+# Copyright (C) 2023 Paranoid Android
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
