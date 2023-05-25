227e1fbc89c4a503503e18d357e45578d438a0d6
Piyush Raj Chouhan <103498040+Pc1598@users.noreply.github.com>
2023-05-25 05:21:09 +0000

    raphael: Import Kernel flags

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -25,5 +25,25 @@ TARGET_2ND_CPU_VARIANT := cortex-a76
 TARGET_BOOTLOADER_BOARD_NAME := msmnile
 TARGET_NO_BOOTLOADER := true
 
+# Kernel
+BOARD_KERNEL_BASE := 0x00000000
+BOARD_KERNEL_CMDLINE := \
+    androidboot.console=ttyMSM0 \
+    androidboot.hardware=qcom \
+    androidboot.usbcontroller=a600000.dwc3 \
+    earlycon=msm_geni_serial,0xa90000 \
+    loop.max_part=7 \
+    lpm_levels.sleep_disabled=1 \
+    service_locator.enable=1 \
+    kpti=off
+#BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
+BOARD_KERNEL_PAGESIZE := 4096
+BOARD_KERNEL_SEPARATED_DTBO := true
+BOARD_RAMDISK_USE_LZ4 := true
+TARGET_KERNEL_CONFIG := raphael_defconfig
+KERNEL_CUSTOM_LLVM := true
+KERNEL_FULL_LLVM := true
+
 # Inherit from proprietary files
 include vendor/xiaomi/raphael/BoardConfigVendor.mk
