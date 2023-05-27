ffc345d0db92cc217a023b1a4621737cde055a1e
pc1598 <piyushchouhan1598@gmail.com>
2023-05-27 08:41:59 +0000

    raphael: Enable fastbootd

--- device.mk
+++ device.mk
@@ -139,6 +139,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.4.vendor
 
+# Fastbootd
+PRODUCT_PACKAGES += \
+    fastbootd
+
 # Fingerprint
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
